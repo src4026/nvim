c451730e462e8a62153ce19753951778db592ab7
Sasha <src4026@proton.me>
2026-05-06 22:58:36 +0530

    Fix treesitter, add latex and markdown lsp support, add lspsaga (breadcrumbs)

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,21 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "texlab", "markdown_oxide", "pyright" }
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+local nvlsp = require "nvchad.configs.lspconfig"
+
+vim.lsp.config("markdown_oxide", {
+  on_init = nvlsp.on_init,
+  capabilities = vim.tbl_deep_extend('force',
+    nvlsp.capabilities,
+    {
+      workspace = {
+        didChangeWatchedFiles = { dynamicRegistration = true }
+      }
+    }
+  ),
+  on_attach = function(client, bufnr)
+    nvlsp.on_attach(client, bufnr)
+  end,
+})
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,28 +1,75 @@
 return {
   {
     "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
     opts = require "configs.conform",
   },
-
-  -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
     config = function()
       require "configs.lspconfig"
     end,
   },
-
-  -- test new blink
-  -- { import = "nvchad.blink.lazyspec" },
-
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "lervag/vimtex",
+    ft = "tex",
+    init = function()
+      vim.g.vimtex_view_method = "zathura"
+      vim.g.vimtex_compiler_method = "latexmk"
+    end,
+  },
+  {
+    "jbyuki/nabla.nvim",
+    ft = "tex",
+    keys = {
+      { "<leader>lp", function() require("nabla").popup() end, desc = "Equation preview" },
+      { "<leader>lt", function() require("nabla").toggle_virt() end, desc = "Toggle inline math" },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim", "lua", "vimdoc",
+        "html", "css", "latex", "markdown",
+        "markdown_inline",
+      },
+    },
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function()
+      local conf = require "nvchad.configs.cmp"
+      for _, source in ipairs(conf.sources) do
+        if source.name == "nvim_lsp" then
+          source.option = {
+            markdown_oxide = {
+              keyword_pattern = [[\(\k\| \|\/\|#\)\+]]
+            }
+          }
+          break
+        end
+      end
+      return conf
+    end,
+  },
+  {
+    "nvimdev/lspsaga.nvim",
+    event = "LspAttach",
+    config = function()
+      require("lspsaga").setup({
+        symbol_in_winbar = {
+        enable = true,
+        separator = " › ",
+        hide_keyword = false,
+        show_file = true,
+        folder_level = 1,
+        color_mode = true,
+        },
+      })
+    end,
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+  },
 }
